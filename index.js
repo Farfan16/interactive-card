e98d8868bb2b5efa2865d0c13d3afd73b63091a2
Farrel Fanny <farrelexamples@gmail.com>
2024-02-22 12:35:21 +0700

    style: Give error message when submitting

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-let cardCreditNumber = document.querySelector("#credit-number");
-let cardName = document.querySelector("#user-name");
-let cardMonthExp = document.querySelector("#month-exp");
-let cardYearExp = document.querySelector("#year-exp");
-let cardCVC = document.querySelector("#card-cvc");
+const cardName = document.querySelector("#user-name");
+const cardCreditNumber = document.querySelector("#credit-number");
+const cardMonthExp = document.querySelector("#month-exp");
+const cardYearExp = document.querySelector("#year-exp");
+const cardCVC = document.querySelector("#card-cvc");
 
-let inputCardNumber = document.querySelector("#card-number");
-let inputName = document.querySelector("#name");
-let inputMonth = document.querySelector("#date-month");
-let inputYear = document.querySelector("#date-year");
-let inputCVC = document.querySelector("#cvc");
+const inputName = document.querySelector("#name");
+const inputCardNumber = document.querySelector("#card-number");
+const inputMonth = document.querySelector("#date-month");
+const inputYear = document.querySelector("#date-year");
+const inputCVC = document.querySelector("#cvc");
+const formId = document.querySelector("#formId");
+
+const errorName = document.querySelector("#errorName");
+const errorNumber = document.querySelector("#errorNumber");
+const errorExpDate = document.querySelector("#errorExpDate");
+const errorCVC = document.querySelector("#errorCVC");
 
 const setCardName = () => {
   if (inputName.value == "") {
@@ -52,4 +58,84 @@ const setCardCVC = () => {
   } else {
     cardCVC.innerHTML = inputCVC.value;
   }
+};
+
+const submitCard = () => {
+  const labelErrorHandler = (isError, labelId, inputId, messageType) => {
+    if (isError) {
+      labelId.classList.add("error");
+      labelId.classList.remove("disable");
+      if (messageType == "blank") {
+        labelId.innerHTML = "Can't be blank";
+      } else if (messageType == "format") {
+        labelId.innerHTML = "Wrong format, numbers only";
+      } else {
+        labelId.innerHTML = "";
+      }
+      inputId.classList.add("error-input");
+    } else {
+      labelId.classList.add("disable");
+      labelId.classList.remove("error");
+      labelId.innerHTML = "";
+      inputId.classList.remove("error-input");
+    }
+  };
+
+  const inputError = (id) => {
+    id.classList.add("error-input");
+  };
+
+  const showError = (id) => {
+    id.classList.add("error");
+    id.classList.remove("disable");
+  };
+
+  const blankError = (id) => {
+    id.innerHTML = "Can't be blank";
+  };
+
+  const handleForm = (e) => {
+    e.preventDefault();
+  };
+  formId.addEventListener("submit", handleForm);
+
+  const numberFilter = /^[\d\s]+$/;
+
+  if (inputName.value == "") {
+    labelErrorHandler(true, errorName, inputName, "blank");
+  } else {
+    labelErrorHandler(false, errorName, inputName);
+  }
+  if (inputCardNumber.value == "") {
+    labelErrorHandler(true, errorNumber, inputCardNumber, "blank");
+  } else if (!numberFilter.test(inputCardNumber.value)) {
+    labelErrorHandler(true, errorNumber, inputCardNumber, "format");
+  } else {
+    console.log(!numberFilter.test(inputCardNumber.value));
+    labelErrorHandler(false, errorNumber, inputCardNumber);
+  }
+  if (inputMonth.value == 0 || inputYear.value == 0) {
+    showError(errorExpDate);
+    blankError(errorExpDate);
+    inputError(inputMonth);
+    inputError(inputYear);
+    if (inputMonth.value != 0) {
+      inputMonth.classList.remove("error-input");
+    }
+    if (inputYear.value != 0) {
+      inputYear.classList.remove("error-input");
+    }
+  } else {
+    errorExpDate.classList.add("disable");
+    errorExpDate.classList.remove("error");
+    errorExpDate.innerHTML = "";
+    inputMonth.classList.remove("error-input");
+    inputYear.classList.remove("error-input");
+  }
+  if (inputCVC.value == 0) {
+    labelErrorHandler(true, errorCVC, inputCVC, "blank");
+  } else {
+    labelErrorHandler(false, errorCVC, inputCVC, "");
+  }
+  console.log("Berhasil yay!");
 };
